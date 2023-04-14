3cec06fe4f20eae2d379b2cef6263451b8a2916c
matesic42 <markomtsc@gmail.com>
2023-04-14 14:50:17 +0200

    Getting Started

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from "react"
+
+const App:FC =() => {  
+  return (
+
+    <h1 className="text-[#e0def4] font-bold font-[Roboto-Slab] text-center text-3xl mt-5">ToDo List</h1>      
+  )
+}
+
+export default App
